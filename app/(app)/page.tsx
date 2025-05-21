f2ae630e38746672029dfa2a76e4a220f6d99582
ahmoin <123082059+ahmoin@users.noreply.github.com>
2025-05-21 08:52:35 -0400

    feat: add ColorfulText component and integrate into HomePage

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,4 +1,5 @@
 import { CircularGallery } from "@/components/circular-gallery";
+import { ColorfulText } from "@/components/colorful-text";
 import { SlackCountText } from "@/components/slack-count-text";
 import Image from "next/image";
 
@@ -19,6 +20,20 @@ export default function HomePage() {
 				<div className="container relative z-10 mx-auto mt-2xl flex max-w-[1057px] flex-col items-center justify-center gap-y-2xl text-center md:mt-lg md:gap-y-3xl text-white font-bold">
 					<h1 className="text-5xl tracking-tight text-balance sm:text-7xl">
 						Welcome to Hack Club
+					</h1>
+					<h2 className="mt-8 text-lg font-medium text-pretty sm:text-xl/8">
+						We are{" "}
+						<span>
+							<SlackCountText />
+						</span>{" "}
+						teen hackers from around the world who code together
+					</h2>
+				</div>
+			</div>
+			<div className="relative flex -mt-(--header-height) max-h-[690px] min-h-[690px] items-center justify-center">
+				<div className="container relative z-10 mx-auto mt-2xl flex max-w-[1057px] flex-col items-center justify-center gap-y-2xl text-center md:mt-lg md:gap-y-3xl text-white font-bold">
+					<h1 className="text-5xl tracking-tight text-balance sm:text-7xl">
+						Discover the <ColorfulText text={"joy of code,"} /> together.
 					</h1>
 					<h2 className="mt-8 text-lg font-medium text-pretty sm:text-xl/8">
 						We are{" "}
