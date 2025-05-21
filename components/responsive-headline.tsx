5317db2428b19c17a9ba250ea2774193642eba09
ahmoin <123082059+ahmoin@users.noreply.github.com>
2025-05-21 18:05:43 -0400

    test: update ResponsiveHeadline component with Balancer for improved text wrapping

--- components/responsive-headline.tsx
+++ components/responsive-headline.tsx
@@ -1,15 +1,18 @@
 "use client";
 
+import Balancer from "react-wrap-balancer";
 import { SlackCountText } from "@/components/slack-count-text";
 
 export function ResponsiveHeadline() {
 	return (
-		<h2 className="mt-8 text-lg font-medium text-balance sm:text-xl/8">
-			We are{" "}
-			<span>
-				<SlackCountText />
-			</span>{" "}
-			teen hackers from around the world who code together
+		<h2 className="mt-8 font-medium text-5xl sm:text-7xl">
+			<Balancer>
+				We are{" "}
+				<span>
+					<SlackCountText />
+				</span>{" "}
+				teen hackers from around the world who code together
+			</Balancer>
 		</h2>
 	);
 }
