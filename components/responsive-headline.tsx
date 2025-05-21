2a57e08ed2e35050d0b94313993fd8066960c9dc
ahmoin <123082059+ahmoin@users.noreply.github.com>
2025-05-21 17:59:39 -0400

    test: try balance text wrap for headline

--- components/responsive-headline.tsx
+++ components/responsive-headline.tsx
@@ -1,32 +1,15 @@
 "use client";
 
 import { SlackCountText } from "@/components/slack-count-text";
-import { useIsMobile } from "@/hooks/use-mobile";
 
 export function ResponsiveHeadline() {
-	const isMobile = useIsMobile();
-
 	return (
-		<>
-			{isMobile ? (
-				<h2 className="text-4xl tracking-tight text-balance leading-tight">
-					<div className="mb-2">
-						We are{" "}
-						<span>
-							<SlackCountText />
-						</span>
-					</div>
-					<div>teen hackers from around the world who code together</div>
-				</h2>
-			) : (
-				<h2 className="text-5xl tracking-tight text-balance sm:text-7xl">
-					We are{" "}
-					<span>
-						<SlackCountText />
-					</span>{" "}
-					teen hackers from around the world who code together
-				</h2>
-			)}
-		</>
+		<h2 className="mt-8 text-lg font-medium text-balance sm:text-xl/8">
+			We are{" "}
+			<span>
+				<SlackCountText />
+			</span>{" "}
+			teen hackers from around the world who code together
+		</h2>
 	);
 }
